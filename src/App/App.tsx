e514fcfffe479c0b6ee4c39e508cf2fd20517174
Anders Nørrelykke <anders@norrelykke.com>
2023-11-30 19:21:20 +0100

    initial type additions

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,33 +1,37 @@
 import React from "react"
-
+import type { ChangeEvent, FC } from "react"
 import { Weather } from "./Weather"
-
-import type { FC } from "react"
+import { QueryClient, QueryClientProvider } from "react-query"
 
 import "./app.css"
 
 export const App: FC = () => {
-    const [ input, setInput ] = 
-        React.useState( null as unknown as string )
+  const queryClient = new QueryClient()
+  const [input, setInput] = React.useState<string>("")
+  const [city, setCity] = React.useState<string>()
 
-    const [ city, setCity ] =
-        React.useState( null as unknown as string )
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void =>
+    setInput(event.target.value)
 
-    const doStuff = ( event: any ) => 
-        setInput( event.target.value )
+  const submitCity = (input: string): void => {
+    setCity(input)
+  }
 
-    console.log( input )
-    
-    return  <>
-                <input 
-                    role="search"
-                    type="text" 
-                    value={ input as unknown as string } 
-                    onChange={ doStuff }  />
-
-                <button onClick={ () => setCity( input ) } >
-                    Show Weather
-                </button>
-
-                < Weather city={ city } />
-            </> }
+  return (
+    <QueryClientProvider client={queryClient}>
+      <input
+        role="search"
+        type="text"
+        value={input}
+        onChange={handleChange}
+        onKeyDown={(e) => {
+          if (e.keyCode === 13) {
+            submitCity(input)
+          }
+        }}
+      />
+      <button onClick={() => submitCity(input)}>Show Weather</button>
+      <Weather city={city} />
+    </QueryClientProvider>
+  )
+}
--- src/App/Weather.tsx
+++ src/App/Weather.tsx
@@ -1,47 +1,69 @@
-import React from "react"
+import { useQuery } from "react-query"
 
-const Weather = ( props: any ) => {
-    const [ info, setInfo ] = React.useState( {
-        city: null,
-        temp: null,
-        icon: null, } )
+type Info = {
+  city: string
+  temp: number
+  icon: string
+  country: string
+}
 
-    console.log( "info" + JSON.stringify( info ) )
+type Location = {
+  name: string
+  lat: number
+  lon: number
+  country: string
+}
 
-    const isPending = React.useRef( false )
+type WeatherType = {
+  main: {
+    temp: number
+  }
+  weather: [{ icon: string }]
+}
 
-    React.useLayoutEffect( () => {
-        if ( props.city !== null || isPending.current === false ) {
+const Weather = ({ city }: { city?: string }) => {
+  // Replacing useState state management and useRef loading tracker with useQuery
+  const { data, error, isLoading } = useQuery<Info, { message: string }>(
+    ["weather", city],
+    async () => {
+      const [location]: [Location] = await fetch(
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=4c4f0b1876954338598a7be96c66527b`
+      ).then((res) => res.json())
+      if (!location) {
+        const error = new Error()
+        error.message = `No location found matching: ${city}`
+        throw error
+      }
+      const weather: WeatherType = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&units=metric&appid=4c4f0b1876954338598a7be96c66527b`
+      ).then((res) => res.json())
+      return {
+        city: location.name,
+        temp: weather.main.temp,
+        icon: `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`,
+        country: location.country,
+      }
+    },
+    { enabled: !!city, retry: 1 }
+  )
 
-        isPending.current = true
+  return (
+    <>
+      {isLoading && <>Loading weather data...</>}
+      <h1>
+        {data?.city} {data?.country && `(${data?.country})`}
+      </h1>
 
-        fetch( `https://api.openweathermap.org/geo/1.0/direct?q=${ props.city }&appid=4c4f0b1876954338598a7be96c66527b` )
-            .then( res => res.json() )
-            .then( data => {
-                console.log( data )
+      {data && (
+        <>
+          <p>{data?.temp && ~~data?.temp} Celcius</p>
+          <img src={data?.icon} alt="Icon" />
+        </>
+      )}
 
-                fetch( `https://api.openweathermap.org/data/2.5/weather?lat=${ data[ 0 ].lat }&lon=${ data[ 0 ].lon }&units=metric&appid=4c4f0b1876954338598a7be96c66527b` )
-                    .then( res => res.json() )
-                    .then( data => {
-                        console.log( data )
-                        
-                        setInfo( {
-                            city: data.name,
-                            temp: data.main.temp,
-                            icon: `https://openweathermap.org/img/wn/${ data.weather[ 0 ].icon }@2x.png` as any, } ) 
-                        
-                        isPending.current = false } ) } ) } }, [ props.city ] )
-
-    return  <>
-                <h1>
-                    { info.city }
-                </h1>
-
-                <p>
-                    { info.temp && ~~ info.temp } Celcius
-                </p>
-
-                <img src={ info.icon as unknown as string } alt="Icon" />
-            </> }
+      {error && error.message}
+    </>
+  )
+}
 
 export { Weather }
